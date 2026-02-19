05012c3752c1e0f16498891d010e88311c3db3f5
shivaprasad0356 <shivaprasadkorakanchi0972@gmail.com>
2026-02-19 13:20:17 +0530

    Update start.sh
    Fix start.sh to copy from CodeDeploy destination directory

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 systemctl start httpd
 systemctl enable httpd
-cp -r /opt/codedeploy-agent/deployment-root/*/deployment-archive/app/* /var/www/html/
+
+cp -r /opt/codedeploy-agent/deployment-root/app/* /var/www/html/
